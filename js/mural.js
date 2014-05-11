24278a43d44e723d1056e60af3e3a3612d13e154
Kawandeep Virdee <whichlight@gmail.com>
2014-05-10 20:18:59 -0400

    adding sparks

--- js/mural.js
+++ js/mural.js
@@ -4,7 +4,12 @@ var socket = io.connect('http://'+window.location.hostname);
 var cells = {};
 stepRate = 30;
 
+var delta = 1, clock = new THREE.Clock();
 var group = new THREE.Object3D();
+
+var speed = 50;
+var particleCloud, sparksEmitter, emitterPos;
+var delta = 1, clock = new THREE.Clock();
 
 
 //x,y,z,color,id
@@ -14,86 +19,239 @@ socket.on('connect', function(){
   console.log('connected');
 });
 
-//create a mobile object for each id, if it doesnt exist else update it
+socket.on('mural', function(data){
+  var d = data.data;
 
-$(document).ready(function(){
-  console.log('socket');
+  if(!(d.id in cells)){
+    cells[d.id] = new Cell(d.id, d.color);
+  }
 
-  socket.on('mural', function(data){
-    var d = data.data;
-
-    if(!(d.id in cells)){
-      cells[d.id] = new Cell(d.id, d.color);
-    }
-
-    cells[d.id].read(d.actions);
-  });
+  cells[d.id].read(d.actions);
 });
 
 
-var particleCount = 1800,
-    particles = new THREE.Geometry(),
-    pMaterial =
-      new THREE.ParticleBasicMaterial({
-        size: 10,
-        transparent: true
-      });
-var particleSystem =
-  new THREE.ParticleSystem(
-    particles,
-    pMaterial);
+//create a mobile object for each id, if it doesnt exist else update it
+
+$(document).ready(function(){
+  initParticleSystem();
+});
+
+
+var initParticleSystem = function(){
+   var particlesLength = 70000;
+   var particles = new THREE.Geometry();
+   var Pool = {
+     __pools: [],
+     // Get a new Vector
+     get: function() {
+       if ( this.__pools.length > 0 ) {
+         return this.__pools.pop();
+       }
+       console.log( "pool ran out!" )
+         return null;
+     },
+
+     // Release a vector back into the pool
+     add: function( v ) {
+       this.__pools.push( v );
+     }
+   };
+
+   for ( i = 0; i < particlesLength; i ++ ) {
+     particles.vertices.push( newpos( Math.random() * 200 - 100, Math.random() * 100 + 150, Math.random() * 50 ) );
+     Pool.add( i );
+   }
+
+   attributes = {
+     size:  { type: 'f', value: [] },
+     pcolor: { type: 'c', value: [] }
+   };
+
+   var sprite = generateSprite();
+   texture = new THREE.Texture( sprite );
+   texture.needsUpdate = true;
+   uniforms = {
+     texture:   { type: "t", value: texture }
+   };
 
 
 
-for(var p = 0; p < particleCount; p++) {
-    var v = new THREE.Vector3(Math.random() * 200 - 100, Math.random() * 100 + 150, Math.random() * 50 );
-    v.isDirty=true;
-    particles.vertices.push(v);
+   var shaderMaterial = new THREE.ShaderMaterial( {
+
+     uniforms: uniforms,
+       attributes: attributes,
+
+       vertexShader: document.getElementById( 'vertexshader' ).textContent,
+       fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+
+       blending: THREE.AdditiveBlending,
+       depthWrite: false,
+       transparent: true
+
+   });
+
+   particleCloud = new THREE.ParticleSystem( particles, shaderMaterial );
+
+   particleCloud.dynamic = true;
+   // particleCloud.sortParticles = true;
+
+   var vertices = particleCloud.geometry.vertices;
+   var values_size = attributes.size.value;
+   var values_color = attributes.pcolor.value;
+
+   for( var v = 0; v < vertices.length; v ++ ) {
+
+     values_size[ v ] = 50;
+     values_color[ v ] = new THREE.Color( 0x000000 );
+
+     particles.vertices[ v ].set( Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY );
+
+   }
+
+   group.add( particleCloud );
+   particleCloud.y = 800;
+
+
+   var hue = 0;
+
+   var setTargetParticle = function() {
+
+     var target = Pool.get();
+     values_size[ target ] = Math.random() * 100 + 50;
+
+     return target;
+
+   };
+
+   var onParticleCreated = function( p ) {
+
+     var position = p.position;
+     p.target.position = position;
+     p.velocity.x = 0;
+     p.velocity.y = 10;
+     var velocity = p.velocity;
+     p.target.velocity= velocity;
+     var target = p.target;
+
+     if ( target ) {
+       if ( hue > 1 ) hue -= 1;
+       emitterpos.x = 0;
+       emitterpos.y = 0;
+       pointLight.position.x = emitterpos.x;
+       pointLight.position.y = emitterpos.y;
+       pointLight.position.z = 10;
+       particles.vertices[ target ] = p.position;
+       values_color[ target ].setHSL( hue, 0.6, 0.1 );
+       pointLight.color.setHSL( hue, 0.8, 0.5 );
+
+     };
+   };
+
+   var onParticleDead = function( particle ) {
+     var target = particle.target;
+     if ( target ) {
+       // Hide the particle
+       values_color[ target ].setRGB( 0, 0, 0 );
+       particles.vertices[ target ].set( Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY );
+
+       // Mark particle system as available by returning to pool
+       Pool.add( particle.target );
+     }
+   };
+
+   var engineLoopUpdate = function() {
+
+   };
+
+   sparksEmitter = new SPARKS.Emitter( new SPARKS.SteadyCounter( 500 ) );
+
+   emitterpos = new THREE.Vector3( 0, 0, 0 );
+
+   sparksEmitter.addInitializer( new SPARKS.Position( new SPARKS.PointZone( emitterpos ) ) );
+   sparksEmitter.addInitializer( new SPARKS.Lifetime( 1, 15 ));
+   sparksEmitter.addInitializer( new SPARKS.Target( null, setTargetParticle ) );
+
+
+   sparksEmitter.addInitializer( new SPARKS.Velocity( new
+         SPARKS.PointZone( new THREE.Vector3( 0, 200, 0 ) ) ) );
+
+   sparksEmitter.addAction( new SPARKS.Age() );
+   sparksEmitter.addAction( new SPARKS.Accelerate( 0, -90, 0 ) );
+   sparksEmitter.addAction( new SPARKS.Move() );
+   sparksEmitter.addAction( new SPARKS.RandomDrift( 90, 100, 500 ) );
+
+
+   sparksEmitter.addCallback( "created", onParticleCreated );
+   sparksEmitter.addCallback( "dead", onParticleDead );
+
+   //weird hack
+   sparksEmitter._actions[0]._easing = TWEEN.Easing.Linear.None;
+   sparksEmitter.start();
+
+   var effectFocus = new THREE.ShaderPass( THREE.FocusShader );
+
+   var effectCopy = new THREE.ShaderPass( THREE.CopyShader );
+   effectFilm = new THREE.FilmPass( 0.5, 0.25, 2048, false );
+
+   var shaderBlur = THREE.TriangleBlurShader;
+   effectBlurX = new THREE.ShaderPass( shaderBlur, 'texture' );
+   effectBlurY = new THREE.ShaderPass( shaderBlur, 'texture' );
+
+   var radius = 15;
+   var blurAmountX = radius / window.innerWidth;
+   var blurAmountY = radius / window.innerHeight;
+
+   hblur = new THREE.ShaderPass( THREE.HorizontalBlurShader );
+   vblur = new THREE.ShaderPass( THREE.VerticalBlurShader);
+
+   hblur.uniforms[ 'h' ].value =  1 / window.innerWidth;
+   vblur.uniforms[ 'v' ].value =  1 / window.innerHeight;
+
+   effectBlurX.uniforms[ 'delta' ].value = new THREE.Vector2( blurAmountX, 0 );
+   effectBlurY.uniforms[ 'delta' ].value = new THREE.Vector2( 0, blurAmountY );
+
+   effectFocus.uniforms[ 'sampleDistance' ].value = 0.99; //0.94
+   effectFocus.uniforms[ 'waveFactor' ].value = 0.003;  //0.00125
+
+   var renderScene = new THREE.RenderPass( scene, camera );
+
+
+   vblur.renderToScreen = true;
+   effectBlurY.renderToScreen = true;
+   effectFocus.renderToScreen = true;
+   effectCopy.renderToScreen = true;
+   effectFilm.renderToScreen = true;
+
+
+
+   function onWindowResize() {
+
+     windowHalfX = window.innerWidth / 2;
+     windowHalfY = window.innerHeight / 2;
+
+     camera.aspect = window.innerWidth / window.innerHeight;
+     camera.updateProjectionMatrix();
+
+     renderer.setSize( window.innerWidth, window.innerHeight );
+
+     hblur.uniforms[ 'h' ].value =  1 / window.innerWidth;
+     vblur.uniforms[ 'v' ].value =  1 / window.innerHeight;
+
+     var radius = 15;
+     var blurAmountX = radius / window.innerWidth;
+     var blurAmountY = radius / window.innerHeight;
+
+     effectBlurX.uniforms[ 'delta' ].value = new THREE.Vector2( blurAmountX, 0 );
+     effectBlurY.uniforms[ 'delta' ].value = new THREE.Vector2( 0, blurAmountY );
+
+   }
+
+
+   window.addEventListener( 'resize', onWindowResize, false );
+  render();
+
 }
-SPARKS.VectorPool.__pools = particles.vertices;
 
-  var counter = new SPARKS.SteadyCounter(500);
-  var emitter = new SPARKS.Emitter( counter );
-
-  var h = 0;
-  emitter.addInitializer(new SPARKS.Target(null, function(){
-    var material = new THREE.ParticleCanvasMaterial( {  program: SPARKS.CanvasShadersUtils.circles , blending:THREE.AdditiveBlending } );
-
-    material.color.setHSL(h, 1, 0.5); //0.7
-    h += 0.001;
-    if (h>1) h-=1;
-
-    var particle = new THREE.Particle( material );
-
-    particle.scale.x = particle.scale.y = Math.random() * 2 +1;
-    group.add( particle );
-
-    return particle;
-  }));
-  emitter.addInitializer( new SPARKS.Position( new SPARKS.PointZone(new THREE.Vector3(0,0,0))));
-  emitter.addInitializer(new SPARKS.Lifetime(1,15));
-  emitter.addInitializer(new SPARKS.Velocity(new SPARKS.PointZone(new THREE.Vector3(0,-5,1))));
-  emitter.addAction( new SPARKS.Age() );
-  emitter.addAction( new SPARKS.Accelerate( 0, 0, -50 ) );
-  emitter.addAction( new SPARKS.Move() );
-  emitter.addAction( new SPARKS.RandomDrift( 90, 100, 2000 ) );
-  emitter.start();
-
-emitter.addCallback("created", function(p) {
-                    var position = p.position;
-                    p.target.position = position;
-                });
-
-                emitter.addCallback("initialized", function(particle) {
-                    var position = p.position;
-                    p.target.position = position;
-                });
-
-                emitter.addCallback("dead", function(particle) {
-                    particle.target.visible = false; // is this a work around?
-                    group.remove(particle.target);
-
-                });
 
 function Cell(id, color){
   this.id = id;
@@ -104,47 +262,24 @@ function Cell(id, color){
   this.timeline = [];
   this.velocity = new THREE.Vector3(0,0,0);
   this.acceleration = new THREE.Vector3(0,0,0);
-
-
-  //create geometry
-  this.geometry = new THREE.CubeGeometry(1,1,1);
-  this.material = new THREE.MeshLambertMaterial( { color: this.color } );
-  this.cube = new THREE.Mesh( this.geometry, this.material );
-  this.cube.scale.x =5;
-  this.cube.scale.y =10;
-  this.cube.scale.z =5;
-  scene.add(this.cube);
-
-  //particles
+  this.activated = false;
+  this.intensity = 0;
+  this.angle;
 
 }
 
 Cell.prototype.update = function(x,y,z, gamma, beta, color){
+  this.intensity = Math.max(x,y,z);
+  this.activated = this.intensity > 0.5;
+  if(this.activated){
+    this.color = color;
 
-
-  var accelVal = Math.max(x,y,z);
-  this.cube.rotation.z =-1*gamma*0.0174532925;
-  this.cube.rotation.x =beta*0.0174532925;
-  this.color = color;
-  this.cube.material.color.set(color);
-
-  var theta = this.cube.rotation.z +Math.PI/2;
-  var phi = this.cube.rotation.x;
-
-  var lx = Math.cos(theta);
-  var ly = Math.sin(theta);
-//  var lz = Math.sin(phi);
-  var lz = 0;
-
-
-  this.acceleration = new THREE.Vector3(0,0,0);
-  this.acceleration.add(vec(lx,ly,2*lz).multiplyScalar(accelVal/20));
-//  this.acceleration = this.acceleration.add(center.multiplyScalar(0.2));
-   this.velocity.add(this.acceleration.multiplyScalar(0.8));
-   this.cube.position.add(this.velocity.clone().multiplyScalar(0.5));
-   this.cube.position = boundingEnv(this.cube.position)
+    var lx = Math.cos(theta);
+    var ly = Math.sin(theta);
+    this.velocity.x = this.intensity * lx;
+    this.velocity.y = this.intensity * ly;
+  }
 }
-
 
 var bounding = function(val){
   var neg = -50;
@@ -159,18 +294,6 @@ var boundingEnv = function(vec){
   vec.y = bounding(vec.y);
   vec.z = bounding(vec.z);
   return vec;
-}
-
-Cell.prototype.toCenter = function(){
-
-  this.cube.rotation.z+=0.01;
-  this.cube.rotation.x+=0.01;
-  this.cube.position.divideScalar(1.01);
-}
-
-Cell.prototype.slow= function(){
-   this.velocity.divideScalar(1.05);
-   this.cube.position.add(this.velocity.clone().multiplyScalar(0.5));
 }
 
 
@@ -189,46 +312,48 @@ Cell.prototype.step = function(){
       var a = this.timeline.shift();
       this.update(a.x, a.y, a.z, a.gamma, a.beta, a.color);
     }
-  } else if(this.velocity.length()>0.1){
-    this.slow();
-} else {
-//  this.toCenter();
-    this.slow();
   }
 }
 
 
+//particle system
 
-function vec( x, y, z ){ return new THREE.Vector3( x, y, z ); }
+
+
+function newpos( x, y, z ){ return new THREE.Vector3( x, y, z ); }
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 150;
+var camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 2000 );
+camera.position.set( 0, 100, 400 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-
-
-
-
-var pointLight = new THREE.PointLight(0xFFFFFF);
-
 // set its position
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 130;
+pointLight = new THREE.PointLight( 0xffffff, 2, 300 );
+pointLight.position.set( 0, 0, 0 );
 
 // add to the scene
 scene.add(pointLight);
-
 scene.add( group);
 
 
+
+
+
+
 function update(){
+  //interface w particles
+/*
   for(var id in cells){
     cells[id].step();
   }
+
+*/
+  delta = speed * clock.getDelta();
+  particleCloud.geometry.verticesNeedUpdate = true;
+  attributes.size.needsUpdate = true;
+  attributes.pcolor.needsUpdate = true;
 }
 
 function render() {
@@ -236,7 +361,6 @@ function render() {
   update();
   renderer.render(scene, camera);
 }
-render();
 
 
 
@@ -268,6 +392,30 @@ function HSVtoRGB(h, s, v) {
         g: Math.floor(g * 255),
         b: Math.floor(b * 255)
     };
+}
+
+var generateSprite = function() {
+
+  var canvas = document.createElement( 'canvas' );
+  canvas.width = 128;
+  canvas.height = 128;
+
+  var context = canvas.getContext( '2d' );
+  context.beginPath();
+  context.arc( 64, 64, 60, 0, Math.PI * 2, false) ;
+
+  context.lineWidth = 0.5;
+  context.stroke();
+  context.restore();
+
+  var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
+  gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
+  gradient.addColorStop( 0.1, 'rgba(255,255,255,1)' );
+  gradient.addColorStop( 0.5, 'rgba(200,200,200,1)' );
+  gradient.addColorStop( 0.9, 'rgba(0,0,0,1)' );
+  context.fillStyle = gradient;
+  context.fill();
+  return canvas;
 }
 
 
