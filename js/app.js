7a7cb8da00149f348a99c9b84657b5b9e619c309
Kawandeep Virdee <whichlight@gmail.com>
2014-06-14 12:41:44 -0500

    tweaks after eyeo preview. added alert for cell phone instructions. centered text on mural

--- js/app.js
+++ js/app.js
@@ -49,6 +49,7 @@ map_range(accelVal, 0, 15, 100,1500);
  */
 
 $(document).ready(function(){
+    infoAlert();
     setup();
 });
 
@@ -526,6 +527,11 @@ var s = function( sketch ) {
   }
 };
 
-$("#info").click(function(){
+var infoAlert = function(){
   alert("CELLULAR by Kawandeep Virdee of New American Public Art. Turn the sound up on your phone.  Hold your thumb on the screen and slowly move your entire phone. Then tilt it and shake it.  Open up cell-flight.com/mural.html on your laptop. You will make sounds, and participate in the visuals.  Note: you will need to lock your phone orientation.");
+
+}
+
+$("#info").click(function(){
+    infoAlert();
 });
